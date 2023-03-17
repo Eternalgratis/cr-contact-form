4030598f6ccdea9c10b2585eea549047705ff4a8
Titilope Apuabi <tyaps002@gmail.com>
2023-03-17 11:52:46 +0100

    Fix linters

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var hamburger = document.querySelector('.hamburger');
-var navMenu = document.querySelector('.nav-menu');
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
